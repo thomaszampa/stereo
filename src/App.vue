2b61b3965c7f11f691548c17daee1ea1cf2fe4d1
thomaszampa <thomas.zampa@gmail.com>
2019-04-18 20:46:17 -0500

    Full screen nav place holder implimented w/vue 2.0 transitions

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,42 @@
   <div id="app">
     <router-view />
     <div class="nav-container">
-      <div class="hamburger">
+      <a href="#" class="hamburger" @click="openNav">
         <div></div>
         <div></div>
         <div></div>
-      </div>
+      </a>
     </div>
+    <transition name="fade">
+      <div class="menu" v-if="navOpen">
+        <div class="menu-container">
+          <a id="button" href="#" @click="openNav"> CLOSE BUTTON</a>
+          <div class="nav">
+            <div class="nav-item">
+              <a class="nav-link" href="#">LINK</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
-import Vue from "vue";
-import BootstrapVue from "bootstrap-vue";
-
-Vue.use(BootstrapVue);
-
 import "./assets/style/nav_style.scss";
 
 export default {
-  name: "App"
+  name: "App",
+  data() {
+    return {
+      navOpen: false
+    };
+  },
+  methods: {
+    openNav() {
+      this.navOpen = !this.navOpen;
+    }
+  }
 };
 </script>
 
